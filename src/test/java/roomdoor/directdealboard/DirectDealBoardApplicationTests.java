84a83dca154039033e85573c602a37a422e5f935
roomdoor <roomdoor.lee@gmail.com>
2022-09-05 15:10:25 +0900

    init

---
+++ src/test/java/roomdoor/directdealboard/DirectDealBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package roomdoor.directdealboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectDealBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
