873802f1795a8d16c1cd42afde05a78662b8d0f2
roomdoor <roomdoor.lee@gmail.com>
2022-09-05 16:43:07 +0900

    <chore> jacoco add
    테스트 커버리지 확인

--- src/main/java/roomdoor/directdealboard/DirectDealBoardApplication.java
+++ src/main/java/roomdoor/directdealboard/DirectDealBoardApplication.java
@@ -9,5 +9,4 @@ public class DirectDealBoardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DirectDealBoardApplication.class, args);
 	}
-
 }
--- src/test/java/roomdoor/directdealboard/DirectDealBoardApplicationTests.java
+++ src/test/java/roomdoor/directdealboard/DirectDealBoardApplicationTests.java
@@ -1,13 +1,7 @@
 package roomdoor.directdealboard;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DirectDealBoardApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
